2c5362776aa6a2e0ad570e505121ba11b75a3567
Oliver Plunkett <otplunkett@gmail.com>
2019-06-10 17:20:56 -0400

    specify node v

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
@@ -11,81 +11,102 @@ import projects from "../projects"
 const projectUrl = project =>
   `/project?name=${project.name.toLowerCase().replace(" ", "-")}`
 
-const ProjectPage = () => {
-  const idx = projects.findIndex(p =>
-    projectUrl(p).includes(window.location.search)
-  )
+export default class ProjectPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
-  const { name, image, copy } = projects[idx]
+  componentDidMount() {
+    this.loadProject()
+  }
 
-  const previousIdx = idx > 0 ? idx - 1 : projects.length - 1
-  const previousProject = projects[previousIdx]
-  const nextIdx = idx === projects.length - 1 ? 0 : idx + 1
-  const nextProject = projects[nextIdx]
+  componentWillReceiveProps() {
+    this.loadProject()
+  }
 
-  return (
-    <Layout>
-      <SEO title={name} />
+  loadProject() {
+    const { search } = window.location
 
-      <div style={{ textAlign: "center" }}>
-        <div
-          style={{
-            position: "fixed",
-            display: "flex",
-            justifyContent: "space-between",
-            top: "50%",
-            marginBottom: "20px",
-            width: "calc(100vw - 20px)",
-            left: "10px",
-          }}
-        >
-          <Link
-            to={projectUrl(previousProject)}
-            style={{ color: "black", textDecoration: `none`, margin: 0 }}
+    const idx = projects.findIndex(p => projectUrl(p).includes(search))
+    const previousIdx = idx > 0 ? idx - 1 : projects.length - 1
+    const nextIdx = idx === projects.length - 1 ? 0 : idx + 1
+
+    const project = projects[idx]
+    const previousProject = projects[previousIdx]
+    const nextProject = projects[nextIdx]
+
+    this.setState({ project, previousProject, nextProject })
+  }
+
+  render() {
+    const { project, previousProject, nextProject } = this.state
+    if (!project) return null
+
+    const { name, copy, image } = project
+
+    return (
+      <Layout>
+        <SEO title={name} />
+
+        <div style={{ textAlign: "center" }}>
+          <div
+            style={{
+              position: "fixed",
+              display: "flex",
+              justifyContent: "space-between",
+              top: "50%",
+              marginBottom: "20px",
+              width: "calc(100vw - 20px)",
+              left: "10px",
+            }}
           >
-            <img
-              style={{
-                width: "60px",
-                height: "60px",
-                transform: "rotate(180deg)",
-                cursor: "pointer",
-                padding: "10px",
-              }}
-              src={arrow}
-            />
-          </Link>
-          <Link
-            to={projectUrl(nextProject)}
-            style={{ color: "black", textDecoration: `none`, margin: 0 }}
+            <Link
+              to={projectUrl(previousProject)}
+              style={{ color: "black", textDecoration: `none`, margin: 0 }}
+            >
+              <img
+                style={{
+                  width: "60px",
+                  height: "60px",
+                  transform: "rotate(180deg)",
+                  cursor: "pointer",
+                  padding: "10px",
+                }}
+                src={arrow}
+              />
+            </Link>
+            <Link
+              to={projectUrl(nextProject)}
+              style={{ color: "black", textDecoration: `none`, margin: 0 }}
+            >
+              <img
+                style={{
+                  width: "60px",
+                  height: "60px",
+                  cursor: "pointer",
+                  padding: "10px",
+                }}
+                src={arrow}
+              />
+            </Link>
+          </div>
+
+          <p
+            style={{
+              fontSize: "1.2em",
+              letterSpacing: "0.5px",
+              margin: "50px 0 30px 0",
+            }}
           >
-            <img
-              style={{
-                width: "60px",
-                height: "60px",
-                cursor: "pointer",
-                padding: "10px",
-              }}
-              src={arrow}
-            />
-          </Link>
+            <b>{name}</b>
+          </p>
+
+          <p style={{ maxWidth: "600px", textAlign: "left", margin: "0 auto" }}>
+            {copy}
+          </p>
         </div>
-
-        <p
-          style={{
-            fontSize: "1.2em",
-            letterSpacing: "0.5px",
-            margin: "50px 0 30px 0",
-          }}
-        >
-          <b>{name}</b>
-        </p>
-
-        <p style={{ maxWidth: "600px", textAlign: "left", margin: "0 auto" }}>
-          {copy}
-        </p>
-      </div>
-    </Layout>
-  )
+      </Layout>
+    )
+  }
 }
-
-export default ProjectPage
